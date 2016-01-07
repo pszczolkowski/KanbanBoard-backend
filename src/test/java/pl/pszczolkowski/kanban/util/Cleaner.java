e24930ed7c031ca4bbd87f315fb65fc2c4b107b8
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2016-01-07 15:43:49 +0100

    Add static method for cleaning all labels after tests

--- src/test/java/pl/pszczolkowski/kanban/util/Cleaner.java
+++ src/test/java/pl/pszczolkowski/kanban/util/Cleaner.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
 import pl.pszczolkowski.kanban.domain.board.repository.BoardRepository;
+import pl.pszczolkowski.kanban.domain.label.repository.LabelRepository;
 import pl.pszczolkowski.kanban.domain.task.repository.ColumnRepository;
 import pl.pszczolkowski.kanban.domain.task.repository.TaskRepository;
 import pl.pszczolkowski.kanban.domain.user.repository.UserRepository;
@@ -17,6 +18,7 @@ public class Cleaner implements ApplicationContextAware {
 	private static BoardRepository boardRepository;
 	private static ColumnRepository columnRepository;
 	private static TaskRepository taskRepository;
+	private static LabelRepository labelRepository;
 	
 	public static void clearUsers() {
 		userRepository.deleteAll();
@@ -34,12 +36,17 @@ public class Cleaner implements ApplicationContextAware {
 		taskRepository.deleteAll();
 	}
 	
+	public static void clearLabels() {
+		labelRepository.deleteAll();
+	}
+	
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		userRepository = applicationContext.getBean(UserRepository.class);
 		boardRepository = applicationContext.getBean(BoardRepository.class);
 		columnRepository = applicationContext.getBean(ColumnRepository.class);
 		taskRepository = applicationContext.getBean(TaskRepository.class);
+		labelRepository = applicationContext.getBean(LabelRepository.class);
 	}
 
 }
